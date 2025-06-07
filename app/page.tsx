1d6403cd48be58bf6053b6583869ca2e08d4e6eb
Gurleen Singh <learning2989@gmail.com>
2025-06-07 12:20:55 -0400

    implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
-  return <Pagination itemCount={100} pageSize={10} currentPage={2} />;
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  const params = await searchParams;
+  return (
+    <Pagination
+      itemCount={100}
+      pageSize={10}
+      currentPage={parseInt(params.page)}
+    />
+  );
 }
 
 export const metadata = {
