cff0f127208a8cbe99d6e01a391bad70653431a5
Gurleen Singh <learning2989@gmail.com>
2025-06-07 13:08:44 -0400

    created latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,8 @@
 import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  const params = await searchParams;
-  return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={parseInt(params.page)}
-    />
-  );
+export default async function Home() {
+  return <LatestIssues />;
 }
 
 export const metadata = {
